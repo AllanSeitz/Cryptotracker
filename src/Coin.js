2566659b53973c59ac5ef62f380581a0e4ceb543
Allan <allan@Allans-MBP.localdomain>
2018-09-26 13:18:17 -0400

    cryptocoin

--- src/Coin.js
+++ src/Coin.js
@@ -3,15 +3,13 @@ class Coin extends Component {
     state = {}
     render() {
         return (
-
             <tr className="data">
-                <td className="name">Name = {this.props.name}</td>
-                <td className="id">ID = {this.props.id}</td>
-                <td className="symbol">Symbol = {this.props.symbol}</td>
-                <td className="rank">Rank = {this.props.rank}</td>
-                <td className="price">Price = {this.props.price}</td>
+                <td className="name">{this.props.name}</td>
+                <td className="id">{this.props.id}</td>
+                <td className="symbol">{this.props.symbol}</td>
+                <td className="rank">{this.props.rank}</td>
+                <td className="price">{this.props.price}</td>
             </tr>
-
         );
     }
 }
--- src/Data.js
+++ src/Data.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react'
-import Coins from "./Coins.json"
+//import Coins from "./Coins.json"
 import Coin from "./Coin"
 class Data extends Component {
-    state = {}
+    constructor(props) {
+        super(props)
+        this.state = { coins: [] }
+    }
+
+    fetchCoins = () => {
+        fetch('https://api.coinmarketcap.com/v2/ticker/?limit=100')
+            .then(response => {
+                return response.json()
+            })
+            .then(myJson => {
+                const newCoins = Object.values(myJson.data)
+                this.setState({ coins: newCoins })
+                console.log(newCoins)
+            })
+    }
+
+    componentDidMount() {
+        this.fetchCoins()
+
+        setInterval(() => {
+            this.fetchCoins()
+        }, 5000)
+    }
+
     render() {
 
-        let coins = Object.keys(Coins.data).map(id => {
-            let coin = Coins.data[id]
-            return <Coin data={coin.data} id={coin.id}
+        let DOM = Object.keys(this.state.coins).map(id => {
+            let coin = this.state.coins[id]
+            return <Coin key={coin.id} id={coin.id}
                 name={coin.name} symbol={coin.symbol}
                 rank={coin.rank} price={coin.quotes.USD.price} />
         })
@@ -16,7 +40,14 @@ class Data extends Component {
             <div>
                 <table>
                     <tbody>
-                        {coins}
+                        {<tr>
+                            <td>Name </td>
+                            <td>ID</td>
+                            <td>Symbol</td>
+                            <td>Rank</td>
+                            <td>Price</td>
+                        </tr>}
+                        {DOM}
                     </tbody>
                 </table>
             </div >
